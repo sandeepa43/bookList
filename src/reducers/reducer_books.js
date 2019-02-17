108888250e53d72b3c1e98f4dbc9e744aad164f0
sandeepa <sandeepasingh686@gmail.com>
2019-02-17 20:43:01 +0530

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return[
+    {title:'The Miracle Morning'},
+    {title:'Think And Grow Rich'},
+    {title:'Power of Subconcious Mind'},
+    {title:'Power of Positive Thinking'}
+  ]
+}
